8900b04f37791c4faf6184c6cb1f77d9f07352c1
Vitor Oliveira <vitor.oliveira@trustly.com>
2020-08-17 23:27:09 -0300

    [LEVEL-1] Nodejs backend with middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -8,26 +9,89 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const validateId = (request, response, next) => {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({message: "Invalid ID"});
+  }
+
+  return next()
+}
+
+app.use("/repositories/:id", validateId);
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const index = repositories.findIndex(repo => repo.id === id)
+  if (index < 0) {
+    return response.status(400).json({message: "ID not found"});
+  }
+
+  const repo = {
+    ...repositories[index],
+    url,
+    title,
+    techs,
+  }
+
+  repositories[index] = repo
+
+  return response.status(200).json(repo)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id)
+  if (index < 0) {
+    return response.status(400).json({message: "ID not found"});
+  }
+
+  repositories.splice(index, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id)
+  if (index < 0) {
+    return response.status(400).json({message: "ID not found"});
+  }
+
+  const updatedRepo = {
+    ...repositories[index],
+    likes: repositories[index].likes + 1,
+  }
+
+  repositories[index] = updatedRepo
+
+  return response.status(200).json(updatedRepo)
 });
 
 module.exports = app;
